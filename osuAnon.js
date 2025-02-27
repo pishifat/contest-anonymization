085511f9376c12863a74d7b7b6114d40a18c768a
pishifat <31551350+pishifat@users.noreply.github.com>
2025-02-27 09:17:00 -0800

    jk

--- osuAnon.js
+++ osuAnon.js
@@ -141,8 +141,6 @@ async function anonymize() {
             } else if (variables.backgrounds && !variables.multipleBeatmaps && type && (type.mime == 'image/jpeg' || type.mime == 'image/png')) {
                 fs.renameSync(`./temp/osz/${folderString}/${file.entryName}`, `./temp/osz/${folderString}/background.${type.ext}`);
                 newOsz.addLocalFile(`./temp/osz/${folderString}/background.${type.ext}`);
-            }if (file.name.includes('osb') && !variables.storyboards) {
-                // do nothing
             } else {
                 newOsz.addLocalFile(`./temp/osz/${folderString}/${file.entryName}`);
             }
